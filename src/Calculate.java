595957246a0f05f31b95ea893eeba53a25c87705
Nathan Huynh <huynhminhnhut322@gmail.com>
2024-06-07 22:46:44 +1000

    update file

--- src/Calculate.java
+++ src/Calculate.java
@@ -12,17 +12,28 @@ public class Calculate {
         this.width = width;
         this.height = height;
     }
+    public int getSide() {return side; }
+    public double getRadius() { return radius; }
+    public double getLength() { return length; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
 
-    public static void volume(int side){
-        return side * side * side;
+    public void setSide(int side) { this.side = side; }
+    public void setRadius(double radius) { this.radius = radius; }
+    public void setLength(double length) { this.length = length; }
+    public void setWidth(double width) { this.width = width; }
+    public void setHeight(double height) { this.height = height; }
+
+    public static double volume(int side){
+        return  side * side * side;
     }
-    public static void volume(double radius){
+    public static double volume(double radius){
         return (4/3) * Math.PI * radius * radius * radius;
     }
-    public static void volume(double length, double width, double height){
+    public static double volume(double length, double width, double height){
         return length * width * height;
     }
-    public static void volume(double height, double radius){
+    public static double volume(double height, double radius){
         return radius * radius * height;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+//
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Cube Volume Calculator
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the side of cube: ");
+        int side = scanner.nextInt();
+        double CubeVolume = Calculate.volume(side);
+        System.out.println("The volume of the cube is: " + CubeVolume);
+        // sphere volume calculator
+        System.out.println("Enter the radius of sphere: ");
+        double radius = scanner.nextDouble();
+        double SphereVolume = Calculate.volume(radius);
+        System.out.println("The volume of the sphere is: " + SphereVolume);
+        // retangular prism volume calculator
+        System.out.println("Enter the length of the retangular: ");
+        double length = scanner.nextDouble();
+        System.out.println("Enter the width of the retangular: ");
+        double width = scanner.nextDouble();
+        System.out.println("Enter the height of the retangular: ");
+        double height = scanner.nextDouble();
+        double RetangularVolume = Calculate.volume(length, width, height);
+        System.out.println("The volume of the retangular is: " + RetangularVolume);
+        // cylinder volume calculator
+        System.out.println("Enter the radius of the cylinder: ");
+        double CylinderRadius = scanner.nextDouble();
+        System.out.println("Enter the height of the cylinder: ");
+        double CylinderHeight = scanner.nextDouble();
+        double CylinderVolume = Calculate.volume(CylinderRadius, CylinderHeight);
+        System.out.println("The volume of the cylinder is: " + CylinderVolume);
+
     }
 }
